9407335da0b6378389c848793fbdcc9b54bc4b98
Raphael Awoyinfa <raphicogit@gmail.com>
2024-01-23 23:54:47 +0100

    feat: add loading page to dashboard

---
+++ src/app/(app)/app/dashboard/loading.tsx
@@ -0,0 +1,26 @@
+import {
+  PageHeader,
+  PageHeaderDescription,
+  PageHeaderHeading,
+} from "@/components/page-header"
+import { Skeleton } from "@/components/ui/skeleton"
+
+export default function DashboardLoading() {
+  return (
+    <div className="mx-auto w-full max-w-4xl space-y-6">
+      <PageHeader>
+        <PageHeaderHeading>My Boards</PageHeaderHeading>
+        <PageHeaderDescription>
+          Here&apos;s a quick overview of your boards
+        </PageHeaderDescription>
+      </PageHeader>
+      <Skeleton className="h-9 w-full" />
+      <section className="grid gap-4 sm:grid-cols-3">
+        <Skeleton className="h-44 w-full" />
+        <Skeleton className="h-44 w-full" />
+        <Skeleton className="h-44 w-full" />
+        <Skeleton className="h-44 w-full" />
+      </section>
+    </div>
+  )
+}
