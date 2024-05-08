195e315d578ea504809c9be246e3e7e99ee73aac
Raphael Awoyinfa <raphicogit@gmail.com>
2024-05-08 15:56:32 +0100

    feat: update database instance

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,11 +1,11 @@
 import { env } from "@/env.mjs"
-import { connect } from "@planetscale/database"
-import { drizzle } from "drizzle-orm/planetscale-serverless"
+import { drizzle } from "drizzle-orm/mysql2"
+import mysql from "mysql2/promise"
 
 import * as schema from "./schema"
 
-const connection = connect({
-  url: env.DATABASE_URL,
+const connection = await mysql.createConnection({
+  uri: env.DATABASE_URL,
 })
 
-export const db = drizzle(connection, { schema })
+export const db = drizzle(connection, { schema, mode: "default" })
